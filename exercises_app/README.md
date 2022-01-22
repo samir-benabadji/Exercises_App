c2b0885c4dd328b0f378920b71e05f3c0cb43383
samir-benabadji <benabadji.samir.2002@gmail.com>
2022-01-22 20:04:27 +0100

    Detail Screen Page
    Detail Screen Page

--- exercises_app/README.md
+++ exercises_app/README.md
@@ -1,1 +1,19 @@
 # Exercises_App
+
+**Packages we are using:**
+
+- flutter_svg: [link](https://pub.dev/packages/flutter_svg)
+
+**Fonts**
+
+- Cairo [link](https://fonts.google.com/specimen/Cairo)
+
+creating an exercise app
+
+### Exercices App Final UI Preview
+
+
+| Main Menu | Detail |
+|--|--|
+| ![Home Page](T1.png) | ![Detail Screen](T3.png) | !
+
--- exercises_app/lib/main.dart
+++ exercises_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:meditation_app/constants.dart';
+import 'package:meditation_app/screens/details_screen.dart';
 import 'package:meditation_app/widgets/bottom_nav_bar.dart';
 import 'package:meditation_app/widgets/category_card.dart';
 import 'package:meditation_app/widgets/search_bar.dart';
@@ -88,7 +89,14 @@ class HomeScreen extends StatelessWidget {
                         CategoryCard(
                           title: "Meditation",
                           svgSrc: "assets/icons/Meditation.svg",
-                          press: () {},
+                          press: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) {
+                                return DetailsScreen();
+                              }),
+                            );
+                          },
                         ),
                         CategoryCard(
                           title: "Yoga",
